611df7ffa05f271608db44f9bb23600d78b31566
Livia Lumi <livia3204@gmail.com>
2025-05-15 20:46:28 -0300

    [FEAT]: Adicionando ProxyPubSub em C#

--- ExecuteProject.bat
+++ ExecuteProject.bat
@@ -1,5 +1,5 @@
+start cmd /k dotnet run --project C:\Users\user\Desktop\rede-social\ProxyPubSub\ProxyPubSub.csproj
 start cmd /k python C:\Users\user\Desktop\rede-social\Broker.py
-start cmd /k python C:\Users\user\Desktop\rede-social\ProxyPubSub.py
 start cmd /k python C:\Users\user\Desktop\rede-social\Server.py
 start cmd /k python C:\Users\user\Desktop\rede-social\InteractiveUser.py
 start cmd /k python C:\Users\user\Desktop\rede-social\InteractiveUser.py
---
+++ ProxyPubSub/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var xpubSocket = new XPublisherSocket())
+        using (var xsubSocket = new XSubscriberSocket())
+        {
+            xpubSocket.Bind("tcp://*:5557");
+            xsubSocket.Bind("tcp://*:5558");
+
+            Console.WriteLine("Proxy iniciado: XSUB <--> XPUB");
+
+            // Cria o proxy entre XSUB e XPUB
+            Proxy proxy = new Proxy(xsubSocket, xpubSocket);
+
+            try
+            {
+                proxy.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro no proxy: {ex.Message}");
+            }
+        }
+    }
+}
