5bc83c5a04fce2b3df3a4e02d75f2a1b6e8324da
Livia Lumi <livia3204@gmail.com>
2025-05-15 03:17:07 -0300

    [FEAT]: Adicionando executável

---
+++ ExecuteProject.bat
@@ -0,0 +1,5 @@
+start cmd /k python C:\Users\user\Desktop\rede-social\Broker.py
+start cmd /k python C:\Users\user\Desktop\rede-social\ProxyPubSub.py
+start cmd /k python C:\Users\user\Desktop\rede-social\Server.py
+start cmd /k python C:\Users\user\Desktop\rede-social\InteractiveUser.py
+start cmd /k python C:\Users\user\Desktop\rede-social\InteractiveUser.py
